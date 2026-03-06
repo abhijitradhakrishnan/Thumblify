73cfeb2021c35de392385c0cedee4506495c3f07
Abhijit Radhakrishnan <peringadanabhijit@gmail.com>
2026-03-06 20:21:12 +0530

    url updated in cors

--- server/server.ts
+++ server/server.ts
@@ -22,7 +22,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-    origin: ['http://localhost:5173', 'http://localhost:3000'],
+    origin: ['http://localhost:5173', 'http://localhost:3000', 'https://thumblify-olive-chi.vercel.app'],
     credentials: true
 }))
 
